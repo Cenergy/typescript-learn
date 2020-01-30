c0fbb87dacfc7dbb68e017d76063d4717821b474
cenergy <673598118@qq.com>
2020-01-30 22:44:41 +0800

    addd

--- hello.ts
+++ hello.ts
@@ -27,3 +27,64 @@ ajax({
     url:'http://www.baidu.com',
     dataType:'json'
 })
+
+
+// 函数类型的接口
+
+interface encrypt{
+    (key:string,value:string):string;
+}
+const md5:encrypt=(key:string,value:string):string =>{
+ return key+value
+}
+
+console.log(md5('123123','12312321321'))
+
+const arr:number[]=[123,456,7899]
+const arr1:Array<string>=['1111']
+interface userArray{
+    [index:number]:string
+}
+const user:userArray=['abcdefg']
+interface userObj{
+    [index:string]:string
+}
+const user1:userObj={
+    abc:'123213',
+    age:'345'
+}
+interface Animal{
+    name:string,
+    eat(str:string):void
+}
+class Dog implements Animal{
+    name:string
+    constructor(name:string){
+        this.name=name
+    }
+    eat(){
+        console.log(this.name+'eat something')
+    }
+}
+
+const d=new Dog('hello')
+d.eat()
+
+
+class User{
+    username:string|undefined;
+    password:string|undefined;
+}
+class MysqlDB<T>{
+    add(info:T):boolean{
+        console.log("TCL: MysqlDB<T> -> info", info)
+        return true
+    }
+}
+
+const u=new User();
+u.username='zhangshan';
+
+u.password='password';
+const db=new MysqlDB<User>();
+db.add(u)
--- js/hello.js
+++ js/hello.js
@@ -19,3 +19,44 @@ ajax({
     url: 'http://www.baidu.com',
     dataType: 'json'
 });
+var md5 = function (key, value) {
+    return key + value;
+};
+console.log(md5('123123', '12312321321'));
+var arr = [123, 456, 7899];
+var arr1 = ['1111'];
+var user = ['abcdefg'];
+var user1 = {
+    abc: '123213',
+    age: '345'
+};
+var Dog = /** @class */ (function () {
+    function Dog(name) {
+        this.name = name;
+    }
+    Dog.prototype.eat = function () {
+        console.log(this.name + 'eat something');
+    };
+    return Dog;
+}());
+var d = new Dog('hello');
+d.eat();
+var User = /** @class */ (function () {
+    function User() {
+    }
+    return User;
+}());
+var MysqlDB = /** @class */ (function () {
+    function MysqlDB() {
+    }
+    MysqlDB.prototype.add = function (info) {
+        console.log("TCL: MysqlDB<T> -> info", info);
+        return true;
+    };
+    return MysqlDB;
+}());
+var u = new User();
+u.username = 'zhangshan';
+u.password = 'password';
+var db = new MysqlDB();
+db.add(u);
